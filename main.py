31fd6e4e61835d4e59801c537ffd7e4a5861b266
nikhilsaini098 <nikhilsaini01012001@gmail.com>
2025-03-18 23:17:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -376,9 +376,9 @@ async def txt_handler(bot: Client, m: Message):
             name = f'{name1[:60]}'
             
             if 'cpvod.testbook.com' in url:
-               url = f"https://dragoapi.vercel.app/video/{url}"
-               url0 = requests.get(f'https://mon-key-3612a8154345.herokuapp.com/get_keys?url={url}', headers={'x-access-token': 'eyJjb3Vyc2VJZCI6IjQ1NjY4NyIsInR1dG9ySWQiOm51bGwsIm9yZ0lkIjo0ODA2MTksImNhdGVnb3J5SWQiOm51bGx9r'}).json()['url']
-                   
+               url = requests.get(f'http://api.masterapi.tech/akamai-player-v3?url={url}', headers={'x-access-token': 'eyJjb3Vyc2VJZCI6IjQ1NjY4NyIsInR1dG9ySWQiOm51bGwsIm9yZ0lkIjo0ODA2MTksImNhdGVnb3J5SWQiOm51bGx9r'}).json()['url']
+               url0 = f"https://dragoapi.vercel.app/video/{url}"
+                
             if "/master.mpd" in url:
                 cmd= f" yt-dlp -k --allow-unplayable-formats -f bestvideo.{quality} --fixup never {url} "
                 print("counted")
@@ -612,9 +612,9 @@ async def text_handler(bot: Client, m: Message):
             name = f'{name1[:20]}'
             
             if 'cpvod.testbook.com' in url:
-               url = f"https://dragoapi.vercel.app/video/{url}"
-               url0 = requests.get(f'https://mon-key-3612a8154345.herokuapp.com/get_keys?url={url}', headers={'x-access-token': 'eyJjb3Vyc2VJZCI6IjQ1NjY4NyIsInR1dG9ySWQiOm51bGwsIm9yZ0lkIjo0ODA2MTksImNhdGVnb3J5SWQiOm51bGx9r'}).json()['url']
-          
+               url = requests.get(f'http://api.masterapi.tech/akamai-player-v3?url={url}', headers={'x-access-token': 'eyJjb3Vyc2VJZCI6IjQ1NjY4NyIsInR1dG9ySWQiOm51bGwsIm9yZ0lkIjo0ODA2MTksImNhdGVnb3J5SWQiOm51bGx9r'}).json()['url']
+               url0 = f"https://dragoapi.vercel.app/video/{url}"
+                
             if "/master.mpd" in url:
                 cmd= f" yt-dlp -k --allow-unplayable-formats -f bestvideo.{quality} --fixup never {url} "
                 print("counted")
