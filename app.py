18465e045be21e71e311b968a689bb3b5970398c
nikhilsaini098 <nikhilsaini01012001@gmail.com>
2025-03-26 00:03:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,9 +33,9 @@ def hello_world():
     </div>
 	<br></br><br></br><br></br>
 	<footer class="bg-dark text-white text-center py-3 mt-5">
-	<center><img loading="lazy" class="object-none object-center" src="https://tinypic.host/images/2025/02/07/DeWatermark.ai_1738952933236-1.png" width="240" height="120">
+	<center><img loading="lazy" class="object-none object-center" src="https://tinypic.host/images/2025/02/07/DeWatermark.ai_1738952933236-1.png" width="30" height="15">
         Powered By SAINI 
-		<img loading="lazy" class="object-none object-center" src="https://tinypic.host/images/2025/02/07/DeWatermark.ai_1738952933236-1.png" width="240" height="120">
+		<img loading="lazy" class="object-none object-center" src="https://tinypic.host/images/2025/02/07/DeWatermark.ai_1738952933236-1.png" width="30" height="15">
 		<div class="footer__copyright">
             <p class="footer__copyright-info">
                 © 2024 Video Downloader. All rights reserved.
