65190cbf343d2f11fe8e8feff360c39a142e37f7
Gittu2raj11 <152615267+Gittu2raj11@users.noreply.github.com>
2025-04-22 16:08:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def hello_world():
 
 <body>
     <div class="container" style="bg-dark text-red text-center py-3 mt-5">
-        <a href="https://github.com/nikhilsaini098" class="card">
+        <a href="https://github.com/Gittu2raj11" class="card">
             <p>
               />▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄<br 
 	      />██░▄▄▄░█░▄▄▀█▄░▄██░▀██░█▄░▄██<br 
@@ -34,7 +34,7 @@ def hello_world():
 	<br></br><br></br><br></br>
 	<footer class="bg-dark text-white text-center py-3 mt-5">
 	<center><img loading="lazy" class="object-none object-center" src="https://tinypic.host/images/2025/02/07/DeWatermark.ai_1738952933236-1.png" width="60" height="30">
-        Powered By SAINI 
+        Powered By Raj
 		<img loading="lazy" class="object-none object-center" src="https://tinypic.host/images/2025/02/07/DeWatermark.ai_1738952933236-1.png" width="60" height="30">
 		<div class="footer__copyright">
             <p class="footer__copyright-info">
